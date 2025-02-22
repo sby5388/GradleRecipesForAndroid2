77484077e0720b5dc3ff2deefa949b0b478961f8
sby5388 <44052699+sby5388@users.noreply.github.com>
2025-02-23 00:16:05 +0800

    create a empty project with agp 7.2.0 gradle 7.4.2

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        maven("https://mirrors.aliyun.com/maven/")// 阿里云 Maven 仓库
+        maven("https://repo.maven.apache.org/maven2") // Maven 中央仓库
+        maven("https://repo.huaweicloud.com/repository/maven/") // 华为 Maven 仓库 1
+        maven("https://mirrors.huaweicloud.com/repository/maven/") // 华为 Maven 仓库 2
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        maven("https://mirrors.aliyun.com/maven/")// 阿里云 Maven 仓库
+        maven("https://repo.huaweicloud.com/repository/maven/") // 华为 Maven 仓库 1
+        maven("https://mirrors.huaweicloud.com/repository/maven/") // 华为 Maven 仓库 2
+        mavenCentral()
+    }
+}
+
+rootProject.name = "巧用Gradle构建Android应用"
+include(":app")
